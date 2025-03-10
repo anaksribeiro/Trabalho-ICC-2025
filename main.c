83a4bdab650251f55fcb2d6a30e3a99683337546
Filinho <115127639+Filinho@users.noreply.github.com>
2025-03-10 16:26:40 -0300

    Update main.c

--- main.c
+++ main.c
@@ -6,14 +6,24 @@ void imprimir_cliente(int id, char nome[100][50], char cpf[100][15]) {
   printf("\nID: %d\nNome: %s\nCPF: %s\n", id, nome[id], cpf[id]);
 }
 
-// Função para cadastrar cliente
+void imprimir_ponto_de_parada(int id, int codigo[5], char cidade[5][50]){
+  printf("\nID: %d\nCodigo: %d\nCidade: %s\n", id, codigo[id], cidade[id]);
+}
+// Funções para cadastrar
 void cadastrar_cliente(int id, char nome[100][50], char cpf[100][15]) {
-
   printf("Insira o nome do cliente: ");
   scanf("%s", nome[id]);
 
   printf("Insira o CPF do cliente: ");
   scanf("%14s", cpf[id]);
+}
+
+void cadastrar_ponto_de_parada(int id, char ponto[5][50], int codigo[5]){
+  printf("Insira o código do ponto de parada:");
+  scanf("%d", &codigo[id]);
+
+  printf("Insira a cidade do ponto de parada:");
+  scanf("%s", ponto[id]);
 }
 
 int main() {
@@ -23,7 +33,11 @@ int main() {
   char cpf_cliente[100][15];
 
   //pontos de parada
+  int index_ponto_de_parada = 0; //max = 5
+  char nome_ponto_de_parada[5][50];
+  int codigo_ponto_de_parada[5];
 
+  //
   int menu = 0;
   while (menu != -1) {
     printf("\nMenu:\n");
@@ -44,7 +58,13 @@ int main() {
         }
       break;
       case 2:
-        if (id)
+        if (index_ponto_de_parada == 5) printf("Número máximo de pontos de partida atingido!\n");
+        else{
+          cadastrar_ponto_de_parada(index_ponto_de_parada, nome_ponto_de_parada, codigo_ponto_de_parada);
+          imprimir_ponto_de_parada(index_ponto_de_parada, codigo_ponto_de_parada,nome_ponto_de_parada);
+          index_ponto_de_parada++;
+        }
+        break;
       case -1:
         printf("Saindo...\n");
       break;
